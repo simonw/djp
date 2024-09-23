015ae5c780dff5adeda8af6bb82e6526b70161ff
Simon Willison <swillison@gmail.com>
2024-09-23 12:36:38 -0400

    Title is DJP

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "djp"
+project = "DJP"
 author = "Simon Willison"
 copyright = "{}, {}".format(datetime.date.today().year, author)
 
