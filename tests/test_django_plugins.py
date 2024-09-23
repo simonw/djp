9bc8d3bd995cb32d6500d9bd637900d7d343971e
Simon Willison <swillison@gmail.com>
2024-09-23 14:03:36 -0400

    Bulked out middleware test, refs #6

--- tests/test_django_plugins.py
+++ tests/test_django_plugins.py
@@ -3,5 +3,9 @@ from django.test.client import Client
 
 def test_django_plugins():
     response = Client().get("/")
+    assert response["X-DJP-Middleware-After"] == "MiddlewareAfter"
+    assert response["X-DJP-Middleware"] == "Middleware"
+    assert response["X-DJP-Middleware-Before"] == "MiddlewareBefore"
     request = response._request
     assert hasattr(request, "_notes")
+    assert request._notes == ["MiddlewareAfter", "Middleware", "MiddlewareBefore"]
